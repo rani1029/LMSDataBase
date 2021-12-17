c987c6fe652ac8b83b549ed13ef73b3840b48303
rani1029 <ranig1029@gmail.com>
2021-12-17 06:22:16 +0530

    felloship candidates table created

--- lmsdb/Lmsdb tables.sql
+++ lmsdb/Lmsdb tables.sql
@@ -24,3 +24,41 @@ creator_user int DEFAULT NULL,
 );
 
 select * from Hired_Candidates;
+
+--Felloship candidates table
+
+CREATE TABLE Fellowship_Candidates (
+id int identity(1,1) primary key NOT NULL,
+first_name varchar(250) NOT NULL,
+middle_name varchar(200) DEFAULT NULL,
+last_name varchar(200) NOT NULL,
+email varchar(100) NOT NULL,
+hired_city varchar(100) NOT NULL,
+degree varchar(100) NOT NULL,
+hired_date date NOT NULL,
+mobile_num bigint NOT NULL,
+permanent_pincode int DEFAULT NULL,
+hired_lab varchar(50) DEFAULT NULL,
+attitude_remark varchar(200) DEFAULT NULL,
+communication_remark varchar(200) DEFAULT NULL,
+knowledge_remark varchar(200) DEFAULT NULL,
+aggregate_remark varchar(200) DEFAULT NULL,
+birth_date date NOT NULL,
+is_birth_date_verified varchar DEFAULT null,
+parent_name varchar(100) DEFAULT NULL,
+parent_occupation varchar(100) NOT NULL,
+parent_mobile_num bigint NOT NULL,
+parent_annual_sal float DEFAULT NULL,
+local_addr varchar(255) NOT NULL,
+permanent_addr varchar(150) DEFAULT NULL,
+photo_path nvarchar(500) DEFAULT NULL,
+joining_date date DEFAULT NULL,
+candidate_status varchar(100) NOT NULL,
+personal_information int DEFAULT 0,
+bank_information varchar(100) DEFAULT null,
+educational_information_filled varchar(100) DEFAULT null,
+document_status varchar(20) DEFAULT NULL,
+remark varchar(150) DEFAULT NULL,
+);
+
+select * from Fellowship_Candidates;
