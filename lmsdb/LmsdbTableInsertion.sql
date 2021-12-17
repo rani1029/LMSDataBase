24a685a3b40a5078e53f2c9fc5d2c4d04cc6da69
rani1029 <ranig1029@gmail.com>
2021-12-17 23:48:44 +0530

    add:insert values in hired candidates and fellooship students

---
+++ lmsdb/LmsdbTableInsertion.sql
@@ -0,0 +1,26 @@
+--insertion
+--insertion into hired_candidates table
+use LMSDb;
+insert into Hired_Candidates values ('Rani','K','Gupta','ranig@gmail.com','mumbai','B.Tech','2019-05-07',9856743645,987654, 'Devloper', 'fine', 'Good', 'Good', 'Great', 'Ok', '2020-05-07',1);
+insert into Hired_Candidates values ('Sourabh','R','kumar','skk@gmail.com','mumbai','B.Tech','2019-05-07',9856743875,987854, 'Devloper', 'fine', 'Good', 'Good', 'Great', 'Ok', '2020-05-07',2),
+('Josh','K','waren','jkw@gmail.com','mumbai','MBA','2019-05-07',985675342,987654, 'manager', 'fine', 'Good', 'Good', 'Great', 'Ok', '2020-05-07',3),
+('Rahul','g','Sharma','rgk@gmail.com','mumbai','B.Tech','2019-05-07',985678945,987879, 'Devloper', 'fine', 'Good', 'Good', 'Great', 'Ok', '2020-05-07',4),
+('Jammie','K','jonas','jkj@gmail.com','mumbai','B.Tech','2019-05-07',6789743645,567654, 'Devloper', 'fine', 'excellent', 'Good', 'Great', 'Ok', '2020-05-07',5);
+
+=================================================================================================================================================
+
+----insertion into Felloship candidates table
+select * from Fellowship_Candidates
+insert into Fellowship_Candidates values('Rahul','g', 'Sharma', 'rgk@gmail.com','mumbai','M.Tech','2018-11-13', 3456785643,567845, 'Backend',
+'Good', 'Good', 'v good', 'Good', '1997-12-13', 'y', 'shyam', 'Clerk', 654378908, 500000.00, 'juhu', 'nasik',
+'photo_path6', '2019-11-13', 'present', 1, 'yes', 'yes', 'Verified', 'Good');
+insert into Fellowship_Candidates values ('nick', 'jim', 'jams', 'ngn@gmail.com ', 'mumbai','M.Tech','2018-11-13', 34598796436,567845, 'fullstack',
+'Good', 'Good', 'v good', 'Good', '1997-12-13', 'y', 'rams', 'Manager', 6543789087, 1500000.00, 'juhu', 'nasik',
+'photo_path4.jpg', '2019-11-13', 'present', 1, 'yes', 'yes', 'not uploaded', 'Good'),('isha', 'raj', 'Sharma', 'igk@gmail.com ', 'delhi','M.Tech','2018-11-13', 8765436436,987845, 'Backend',
+'Good', 'Good', 'v good', 'Good', '1997-12-13', 'y', 'amar sharma', 'farmer', 6543789087, 500000.00, 'cp', 'luknow',
+'photo_path3.jpg', '2019-11-13', 'present', 1, 'no', 'yes', 'Verified', 'Good'),('rocky', '', 'singh', 'rgs@gmail.com ', 'delhi','media comm','2018-11-13', 85673643621,943215, 'Content writer',
+'Good', 'Good', 'v good', 'Good', '1997-12-13', 'y', 'santhosh singh', 'Salesman', 6543789087, 70000.00, 'cp', 'luknow',
+'photo_path3.jpg', '2019-11-13', 'present', 1, 'no', 'yes','Verified', 'Good');
+
+
+
