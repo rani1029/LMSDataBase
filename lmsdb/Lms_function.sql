32913d52487bd7ae7c2509ca60943edd4cc958e1
rani1029 <ranig1029@gmail.com>
2021-12-21 02:46:44 +0530

    system function ,scalar function implemented

--- lmsdb/Lms_function.sql
+++ lmsdb/Lms_function.sql
@@ -1,18 +1,40 @@
---function
+-- system function
 
+--aggregate function
+--1 avg()
 
-CREATE PROCEDURE totalcandidats (
-    @id INT,
-    @total_count INT OUTPUT
-) AS
-BEGIN
-    SELECT 
-       id
-       
-    FROM
-        Fellowship_Candidates
-    WHERE
-        id = @id;
+select AVG(parent_annual_sal) avgsalary
+from Fellowship_Candidates;
 
-    SELECT @total_count = COUNT();
-END;
+---2 count()
+
+SELECT
+    COUNT(*) count
+FROM
+    Fellowship_Candidates
+WHERE
+    parent_annual_sal > 40000;
+
+----3 max()
+
+select MAX(id) maxsalary
+ from Fellowship_Candidates;
+
+ =====================================================================================================================================================================================
+ --user defined function 
+--Create scalar function to get emp firstnamw name 
+
+Create function getcandidate
+(
+@candidate varchar(50)
+)
+returns varchar(101)
+As
+Begin return (Select @candidate);
+end 
+
+select dbo.getcandidate(first_name) as candidatename from Fellowship_Candidates;
+--execute function
+ select email from getcandidates();
+
+ ==============================================================================================================================================================================
