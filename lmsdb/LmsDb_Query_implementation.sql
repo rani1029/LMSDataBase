24515231d617721e715a25aaa84b14534714b5a9
rani1029 <ranig1029@gmail.com>
2021-12-21 01:09:18 +0530

    right,outer joins,aggregate function implemented

--- lmsdb/LmsDb_Query_implementation.sql
+++ lmsdb/LmsDb_Query_implementation.sql
@@ -1,4 +1,6 @@
 --inner join
+use LMSDb;
+
 select Fellowship_Candidates.first_name,Fellowship_Candidates.email,Candidates_Personal_det_check.field_name
 from Fellowship_Candidates inner join Candidates_Personal_det_check on Fellowship_Candidates.id=Candidates_Personal_det_check.candidate_id;
 
@@ -20,5 +22,10 @@ select Fellowship_Candidates.degree,Candidate_Bank_Det.account_num
 from Fellowship_Candidates full outer join Candidate_Bank_Det on Fellowship_Candidates.id=Candidate_Bank_Det.candidate_id
 order by Fellowship_Candidates.first_name;
 
---union
+--aggregate function
+
+select count(*) notprovided
+from Fellowship_Candidates.email
+where parent_annual_sal=0;
+
 
