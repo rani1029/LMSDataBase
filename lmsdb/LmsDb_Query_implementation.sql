35df8c6027f763113f926128e834b69eed406753
rani1029 <ranig1029@gmail.com>
2021-12-20 12:59:59 +0530

    add:joins implemented on felloship candidate,student details table

---
+++ lmsdb/LmsDb_Query_implementation.sql
@@ -0,0 +1,24 @@
+--inner join
+select Fellowship_Candidates.first_name,Fellowship_Candidates.email,Candidates_Personal_det_check.field_name
+from Fellowship_Candidates inner join Candidates_Personal_det_check on Fellowship_Candidates.id=Candidates_Personal_det_check.candidate_id;
+
+-----left join
+
+select Fellowship_Candidates.first_name,Fellowship_Candidates.email,Candidate_Bank_Det.addhaar_num
+from Fellowship_Candidates left join Candidate_Bank_Det on Fellowship_Candidates.id=Candidate_Bank_Det.candidate_id
+order by Fellowship_Candidates.first_name;
+
+---right join
+
+select Fellowship_Candidates.first_name,Fellowship_Candidates.email,Candidate_Bank_Det.addhaar_num
+from Fellowship_Candidates right join Candidate_Bank_Det on Fellowship_Candidates.id=Candidate_Bank_Det.candidate_id
+order by Fellowship_Candidates.first_name;
+
+--full outer join 
+
+select Fellowship_Candidates.degree,Candidate_Bank_Det.account_num
+from Fellowship_Candidates full outer join Candidate_Bank_Det on Fellowship_Candidates.id=Candidate_Bank_Det.candidate_id
+order by Fellowship_Candidates.first_name;
+
+--union
+
