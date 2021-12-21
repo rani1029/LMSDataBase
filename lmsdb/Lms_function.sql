34d27d86963963fbef8a75fd45da123ef9eccb80
rani1029 <ranig1029@gmail.com>
2021-12-21 13:32:19 +0530

    aggregate function avg(),max() with order by, group by ,having implemented

--- lmsdb/Lms_function.sql
+++ lmsdb/Lms_function.sql
@@ -6,19 +6,48 @@
 select AVG(parent_annual_sal) avgsalary
 from Fellowship_Candidates;
 
----2 count()
+--avg with groub by 
+
+select AVG(parent_annual_sal) avgsalary
+from Fellowship_Candidates
+group by hired_lab;
+
+---count() with group by
 
 SELECT
-    COUNT(*) count
+    COUNT(id) totalCandidates,hired_city
 FROM
     Fellowship_Candidates
 WHERE
-    parent_annual_sal > 40000;
+    parent_annual_sal > 80000 group by hired_city;
 
-----3 max()
+---count() with group by,order by 
 
-select MAX(id) maxsalary
- from Fellowship_Candidates;
+SELECT
+    COUNT(id) totalCandidates,hired_city
+FROM
+    Fellowship_Candidates
+WHERE
+    parent_annual_sal > 40000 group by hired_city order by count(parent_annual_sal);
+
+----max()
+
+select MAX(id) maxsalary,degree
+ from Fellowship_Candidates
+ group by degree;
+
+ ---max() with having,group by 
+
+ select MAX(parent_annual_sal) maxsalary,degree
+ from Fellowship_Candidates
+ group by degree
+ HAVING COUNT(id) > 2;
+
+ ---order by
+
+ SELECT * FROM Fellowship_Candidates
+ORDER BY id;
+
 
  =====================================================================================================================================================================================
  --user defined function 
