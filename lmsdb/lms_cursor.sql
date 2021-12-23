83383c61ef59e1307d3d6fe786814cea3b96b0d6
rani1029 <ranig1029@gmail.com>
2021-12-23 17:13:09 +0530

    static cursor done for user joins table

--- lmsdb/lms_cursor.sql
+++ lmsdb/lms_cursor.sql
@@ -1,12 +1,16 @@
 --- scroll cursor 
 
-declare Retrievecursor cursor scroll for select email from User_Details
+declare Retrievecursor cursor scroll 
+--selecting row 
+for select email from User_Details
 
 declare @candidateEmail varchar(50);
 -----Open cursor
 open Retrievecursor;
 -----Fetch data
 fetch first from Retrievecursor into @candidateEmail; 
+--do query on selected row on whchi have cursor now 
+--typecast as printing as string
 print 'Candidate email: ' +cast(@candidateEmail as varchar(50))
 --close cursor 
 close Retrievecursor
@@ -28,3 +32,25 @@ fetch next from ForwardOnlycursor
 close ForwardOnlycursor
 --Deallocate the cursor
 deallocate ForwardOnlycursor
+
+===============================================================================================================================================================
+---static cursor
+
+declare cursorjoinsTable 
+
+--cursor for joins table
+CURSOR  STATIC for SELECT Fellowship_Candidates.degree,Fellowship_Candidates.birth_date,Candidate_Bank_Det.account_num 
+FROM Fellowship_Candidates
+ LEFT JOIN Candidate_Bank_Det on Fellowship_Candidates.id=Candidate_Bank_Det.id;
+
+open cursorjoinsTable;
+
+--next cursor
+fetch next from cursorjoinsTable
+
+close cursorjoinsTable
+
+--Deallocate the cursor
+deallocate cursorjoinsTable
+
+
