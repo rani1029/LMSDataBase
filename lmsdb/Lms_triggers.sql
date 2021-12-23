d99f3343716036ae53926e77ea069028e61c5a31
rani1029 <ranig1029@gmail.com>
2021-12-23 07:40:54 +0530

    instead of triggers done for insert

--- lmsdb/Lms_triggers.sql
+++ lmsdb/Lms_triggers.sql
@@ -28,3 +28,19 @@ BEGIN
  insert into Fellowship_Candidates values('lira','g', 'Sharma', 'lisds@gmail.com','mumbai','M.Tech','2018-11-13', 3452385643,567845, 'Backend',
 'Good', 'Good', 'v good', 'Good', '1997-12-13', 'y', 'kim', 'Clerk', 64568667808, 500000.00, 'juhu', 'nasik',
 'photo_path8', '2019-11-13', 'present', 1, 'yes', 'yes', 'Verified', 'Good');
+
+=========================================================================================================================================================
+
+--instead of triggers on insert
+
+Create TRIGGER tr_InsertTrigger
+ON User_Roles
+INSTEAD OF INSERT
+AS
+BEGIN
+	print 'Can not add values'
+End
+
+select * from user_roles;
+
+insert into user_roles values (8,'engineer');
